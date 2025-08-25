ade5a246939cfda053a246e0b6a702994fc9f236
trung3011 <tranmaitrung30112004@gmail.com>
2025-08-25 08:46:01 +0700

    part1

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+        var worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPoint.z = 0;
+        transform.position = worldPoint;
+
+
+
+    }
+}
